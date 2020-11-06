392db7d2980117c7e27069ec757465358abdfba3
kristofurus <krzysztof.ladaa@gmail.com>
2020-11-06 18:30:11 +0100

    Created basic settings page.

---
+++ app/src/main/java/com/example/myapplication/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.preferences, new SettingsFragment())
+                .commit();
+    }
+}
