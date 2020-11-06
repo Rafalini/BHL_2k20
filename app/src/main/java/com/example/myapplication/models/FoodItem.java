b885fb82617f55dad9722322d3a237c7f358e8a3
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2020-11-06 19:00:08 +0100

    Started working on item list on main page

---
+++ app/src/main/java/com/example/myapplication/models/FoodItem.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.models;
+
+import java.util.Date;
+
+public class FoodItem {
+    String name;
+    Date expirationDate;
+
+    public FoodItem(String name, Date expirationDate) {
+        this.name = name;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
