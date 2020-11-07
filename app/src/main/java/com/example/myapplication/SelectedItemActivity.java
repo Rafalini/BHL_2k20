3a357b45435a0efc62cbe40bb40f962975861f8e
kristofurus <krzysztof.ladaa@gmail.com>
2020-11-07 12:53:51 +0100

    updated selected item view

--- app/src/main/java/com/example/myapplication/SelectedItemActivity.java
+++ app/src/main/java/com/example/myapplication/SelectedItemActivity.java
@@ -25,10 +25,10 @@ public class SelectedItemActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String data = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         String name = data.substring(0, data.indexOf("\n"));
-        String date = data.substring(data.indexOf("\n")+1);
+        //String date = data.substring(data.indexOf("\n")+1);
         getSupportActionBar().setTitle(name);
-        TextView textView = findViewById(R.id.expirationalDateValue);
-        textView.setText(date);
+        //TextView textView = findViewById(R.id.expirationalDateValue);
+        //textView.setText(date);
 
         WebView myWebView = (WebView) findViewById(R.id.CookbookPage);
         String url = "https://www.kwestiasmaku.com/szukaj?search_api_views_fulltext=" + name;
