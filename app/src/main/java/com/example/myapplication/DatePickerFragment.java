e37835194d9aad6fe5421b7753425411ce7cf341
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2020-11-07 03:47:25 +0100

    Implemented adding item to database with calendar date choice

---
+++ app/src/main/java/com/example/myapplication/DatePickerFragment.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Date date = new Date(year-1900,month,dayOfMonth);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = dateFormat.format(date);
+        TextView dateDisplayText = getActivity().findViewById(R.id.dateDisplayText);
+        String displayDate = "Expiration date: " + dateString;
+        dateDisplayText.setText(displayDate);
+    }
+}
--- app/src/main/java/com/example/myapplication/NewElementFragment.java
+++ app/src/main/java/com/example/myapplication/NewElementFragment.java
@@ -1,12 +1,18 @@
 package com.example.myapplication;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -15,12 +21,15 @@ import com.example.myapplication.models.FoodItem;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class NewElementFragment extends Fragment {
 
     private DatabaseHelper dbHelper;
-
+    private String foodName;
+    private String date;
+    private DialogFragment calendarDialogFragment;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -28,28 +37,35 @@ public class NewElementFragment extends Fragment {
     ) {
         // Inflate the layout for this fragment
         dbHelper = new DatabaseHelper(getActivity().getApplicationContext());
+        calendarDialogFragment = new DatePickerFragment();
         return inflater.inflate(R.layout.new_element_fragment, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        view.findViewById(R.id.selectDateButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendarDialogFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+            }
+        });
+
         view.findViewById(R.id.confirmButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText nameText = getView().findViewById(R.id.ProductNameInput);
-                EditText dateText = getView().findViewById(R.id.ExpirationDateInput);
-
                 String foodName = nameText.getText().toString();
-                Date foodExpirationDate = null;
+
+                TextView dateText = getView().findViewById(R.id.dateDisplayText);
+                String foodExpirationDate = dateText.getText().toString().substring(17);
                 try {
-                    foodExpirationDate = new SimpleDateFormat("yyyy-MM-dd").parse(dateText.getText().toString());
+                    Date expirationDate = new SimpleDateFormat("yyyy-MM-dd").parse(foodExpirationDate);
+                    FoodItem newEntry = new FoodItem(foodName, expirationDate);
+                    dbHelper.addData(newEntry);
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
-                FoodItem newEntry = new FoodItem(foodName, foodExpirationDate);
-
-                dbHelper.addData(newEntry);
 
                 NavHostFragment.findNavController(NewElementFragment.this)
                         .navigate(R.id.action_NewElementFragment_to_MainFragment);
