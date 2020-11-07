9e480ac985027bea99f4e4663f86d7a8ec2595ec
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2020-11-07 05:10:23 +0100

    ListItems are now deletable

--- app/src/main/java/com/example/myapplication/models/FoodItem.java
+++ app/src/main/java/com/example/myapplication/models/FoodItem.java
@@ -3,10 +3,12 @@ package com.example.myapplication.models;
 import java.util.Date;
 
 public class FoodItem {
+    String ID;
     String name;
     Date expirationDate;
 
-    public FoodItem(String name, Date expirationDate) {
+    public FoodItem(String ID, String name, Date expirationDate) {
+        this.ID = ID;
         this.name = name;
         this.expirationDate = expirationDate;
     }
@@ -25,5 +27,13 @@ public class FoodItem {
 
     public void setExpirationDate(Date expirationDate) {
         this.expirationDate = expirationDate;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
     }
 }
